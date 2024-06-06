a9686ac20145f34b7c79a7aef5fa9d758324b0f2
linux <yiguang.mei@etu.utc.fr>
2024-06-06 22:27:27 +0200

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/myg/sy31_projet/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/myg/workspace/devel;/opt/ros/noetic'.split(';') if '/home/myg/workspace/devel;/opt/ros/noetic' != '' else []
---
+++ build/lidar_mapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/myg/sy31_projet/src/lidar_mapping
+# Build directory: /home/myg/sy31_projet/build/lidar_mapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
