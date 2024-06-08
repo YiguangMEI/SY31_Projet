a6822f931780fde605c3fdc3f7a03e786a0306c0
vertaubi <dev@antaluca.com>
2024-06-08 11:27:38 +0200

    removed build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/myg/sy31_projet/src
-# Build directory: /home/myg/sy31_projet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lidar_mapping")
-subdirs("odometry")
--- build/lidar_mapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/myg/sy31_projet/src/lidar_mapping
-# Build directory: /home/myg/sy31_projet/build/lidar_mapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
